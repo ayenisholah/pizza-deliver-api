8f6eaa39ed8e2b7b19cae0b0556fd064a062b8ac
Shola Ayeni <ayenisholah@yahoo.com>
2020-01-12 09:16:21 +0100

    created order handler method to add items to cart

--- handlers/orderHandlers.js
+++ handlers/orderHandlers.js
@@ -6,12 +6,9 @@ var orderHandler = {};
 
 orderHandler.getMenuItems = (data, callback) => {
   const token = data.payload.token || data.headers.token;
-  // Lookup the token
   _data.read("tokens", token, (err, tokenData) => {
     if (!err && tokenData) {
-      // check if the token is stil valid
       if (tokenData.expires > Date.now()) {
-        // Proceed
         callback(200, { menu });
       } else {
         callback(403, {
@@ -23,4 +20,131 @@ orderHandler.getMenuItems = (data, callback) => {
       callback(403, { success: false, error: "access denied, invalid token" });
     }
   });
+};
+
+/**
+ * @data - menu item && quantity
+ * @returns cart object
+ */
+
+orderHandler.addToCart = function addToCart(data, callback) {
+  var item =
+    typeof data.payload.item == "string" && data.payload.item.trim().length > 0
+      ? data.payload.item.toUpperCase()
+      : false;
+  var quantity =
+    typeof data.payload.quantity == "number" && data.payload.quantity > 0
+      ? data.payload.quantity
+      : 1;
+
+  if (item && quantity) {
+    var token =
+      typeof data.headers.token == "string" ? data.headers.token : false;
+    _data.read("tokens", token, function tokenCallback(err, tokenData) {
+      if (!err && tokenData) {
+        var userEmail = tokenData.email;
+
+        _data.read("users", userEmail, function userCallback(err, userData) {
+          if (!err && userData) {
+            var userCart =
+              typeof userData.cart == "object" ? userData.cart : {};
+
+            userData.cart = userCart;
+            var menuList = Object.values(menu);
+            var menuItems = menuList.map(getMenu);
+
+            if (menuItems.indexOf(item) > -1) {
+              var selectedItems =
+                typeof userCart.selectedItems == "object" &&
+                userCart.selectedItems instanceof Array
+                  ? userCart.selectedItems
+                  : [];
+
+              var cartTotal =
+                typeof userCart.cartTotal == "number" && userCart.cartTotal > 0
+                  ? userCart.cartTotal
+                  : 0;
+
+              var { price } = menuList.find(findItem);
+              price *= quantity;
+              var cartObject = {
+                item,
+                quantity,
+                price
+              };
+
+              userData.cart.selectedItems = selectedItems;
+              userData.cart.cartTotal = cartTotal;
+
+              var cartList = userData.cart.selectedItems;
+              var cartItemList = cartList.map(getMenuItemList);
+
+              if (cartItemList.indexOf(item) > -1) {
+                var selected = cartList.find(function(str) {
+                  return str.item == item;
+                });
+                selected.quantity += quantity;
+                let { price } = menuList.find(findItem);
+                selected.price = price * selected.quantity;
+              } else {
+                userData.cart.selectedItems.unshift(cartObject);
+              }
+
+              var cartItemPrices = cartList.map(getCartPrices);
+              cartTotal = cartItemPrices.reduce(reducer);
+              userData.cart.cartTotal = cartTotal;
+
+              _data.update("users", userEmail, userData, function(err) {
+                if (!err) {
+                  callback(200, { success: true, userCart });
+                } else {
+                  callback(500, {
+                    Error: "Could not add item to cart"
+                  });
+                }
+              });
+            } else {
+              callback(400, {
+                success: false,
+                error:
+                  "You have selected an item that does not exist. check api/menu to get menu list"
+              });
+            }
+          } else {
+            callback(404, {
+              success: false,
+              error: "could not find the specied user"
+            });
+          }
+        });
+      } else {
+        callback(403, { success: false, error: "invalid or missing token" });
+      }
+    });
+  } else {
+    callback(400, {
+      success: false,
+      error: "Missing required fields, check api/menu to get menu list"
+    });
+  }
+
+  function getMenu(item) {
+    return item.name;
+  }
+
+  function findItem(menu) {
+    return menu.name == item;
+  }
+
+  function getMenuItemList(menu) {
+    return menu.item;
+  }
+
+  function getCartPrices(menu) {
+    return menu.price;
+  }
+
+  function reducer(acc, curr) {
+    return acc + curr;
+  }
 };
